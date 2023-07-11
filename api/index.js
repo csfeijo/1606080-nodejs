1ed5490ffe3888f2d4f7d2bfe21349918fe70699
Cicero, Feijo <cicero.feijo@gmail.com>
2023-07-11 17:23:40 -0300

    Add Swagger UI

--- api/index.js
+++ api/index.js
@@ -1,12 +1,70 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import departamentosRoutes from './src/routes/departamentosRoutes.js'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsdoc from 'swagger-jsdoc'
 
+import departamentosRoutes from './src/routes/departamentosRoutes.js'
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded( { extended: true } ))
 
 departamentosRoutes(app)
+
+
+
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.3',
+    info: {
+      title: 'Nome da sua API',
+      version: '1.0.0',
+      description: 'Descrição da sua API'
+    },
+    host: 'localhost',
+    basePath: '/',
+    servers: [
+      {
+        url: 'http://localhost:3033', // Ajuste a URL base da sua API
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+  // paths: {
+  //   '/api/departamentos': {
+  //     post: {
+  //       tag: ['departamento'],
+  //       summary: 'Adicionar um novo departamento',
+  //       description: '',
+  //       operationId: 'addDepto',
+  //       consumes: ['application/json'],
+  //       parameters: [
+  //         {
+  //           in: 'body',
+  //           name: 'body',
+  //           description: 'Objeto Departamento que precisa ser adicionado',
+  //           required: true,
+  //           schema: {
+  //             $ref: '#/definitions/Departamentos',
+  //           },
+  //         },
+  //       ],
+  //       responses: {
+  //         200: {
+  //           description: 'Sucesso',
+  //         },
+  //         400: {
+  //           description: 'Parametros faltantes',
+  //         },
+  //       },
+  //     }
+  //   }
+  // }
+}
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.get('/', (req, res) => {
   res.send('Welcome to API')
--- api/src/routes/departamentosRoutes.js
+++ api/src/routes/departamentosRoutes.js
@@ -4,11 +4,73 @@ const departamentosRoutes = (app) => {
 
   const base = '/api'
 
+  /**
+   * @swagger
+   * components:
+   *   schemas:
+   *     Departamentos:
+   *       type: object
+   *       required:
+   *         - nome
+   *         - sigla
+   *       properties:
+   *         nome:
+   *           type: string
+   *           descriptiom: depto nome  
+   *         sigla:
+   *           type: string
+   *           descriptiom: depto sigla
+   *       example:
+   *         nome: sample_depto
+   *         sigla: smp 
+   */
+
+  /**
+   * @swagger
+   *
+   * /api/departamentos:
+   *   get:
+   *     description: Lista todos departamentos
+   *     produces:
+   *       - text/json
+   *     responses:
+   *       200:
+   *         description: Adiciona um registro na entidade de departamentos
+   */
   app.get(`${base}/departamentos`, async (req, res) => {
     const [rows] = await con.query('SELECT * FROM DEPARTAMENTOS')
     res.json(rows)
   })
 
+  app.get(`${base}/departamentos/:id_departamento`, async (req, res) => {
+    const { id_departamento } = req.params
+    const [rows] = await con.query('SELECT * FROM DEPARTAMENTOS WHERE id_departamento = ?', [id_departamento])
+    res.json(rows)
+  })
+
+
+  /**
+   * @swagger
+   *
+   * /api/departamentos:
+   *   post:
+   *     summary: Cadastra um departamento
+   *     produces:
+   *       - application/json
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/Departamentos'
+   *     responses:
+   *       200:
+   *         description: Adiciona um novo departamento na base
+   *       400:
+   *         description: Ausência de algum parametro
+   *       500:
+   *         description: Erro interno do servidor
+   */
   app.post(`${base}/departamentos`, async (req, res) => {
     const { nome, sigla } = req.body
 
@@ -32,6 +94,104 @@ const departamentosRoutes = (app) => {
     }
   })
 
+  /**
+   * @swagger
+   *
+   * /api/departamentos/{id_departamento}:
+   *   patch:
+   *     summary: Atualiza um departamento
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - in: path
+   *         name: id_departamento
+   *         description: ID do departamento a ser atualizado
+   *         required: true
+   *         schema:
+   *           type: string
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/Departamentos'
+   *     responses:
+   *       200:
+   *         description: Adiciona um novo departamento na base
+   *       400:
+   *         description: Ausência de algum parametro
+   *       500:
+   *         description: Erro interno do servidor
+   */
+  app.patch(`${base}/departamentos/:id_departamento`, async (req, res) => {
+    const { id_departamento } = req.params
+    const { nome, sigla } = req.body
+
+    if (!id_departamento) {
+      res.status(400).json({ message: 'One or more fields are unset.'})
+    }
+    
+    const departamento = {}
+    if (nome) departamento.nome = nome
+    if (sigla) departamento.sigla = sigla
+    
+    try {
+      const updateQuery = 'UPDATE DEPARTAMENTOS SET ? WHERE id_departamento = ?'
+      const [result] = await con.query(updateQuery, [departamento, id_departamento])
+
+
+      if (result.affectedRows === 0) {
+        return res.status(404).json({ message: 'Record not found.' })
+      }
+
+      res.status(201).json({ message: 'Record updated.', details: result})
+    } catch(e) {
+      res.status(500).json({ message: 'Error on update the record.', exception: e})
+    }
+  })
+
+  /**
+   * @swagger
+   *
+   * /api/departamentos/{id_departamento}:
+   *   delete:
+   *     description: Remove um departamento
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - in: path
+   *         name: id_departamento
+   *         description: ID do departamento a ser atualizado
+   *         required: true
+   *         schema:
+   *           type: string
+   *     responses:
+   *       200:
+   *         description: Insere um depto. no banco
+   *       400:
+   *         description: Parametro faltante
+   *       404:
+   *         description: Não encontrado
+   */
+  app.delete(`${base}/departamentos/:id_departamento`, async (req, res) => {
+    const { id_departamento } = req.params
+
+    if (!id_departamento) {
+      res.status(400).json({ message: 'One or more fields are unset.'})
+    }
+    try {
+      const [result] = await con.query('DELETE FROM DEPARTAMENTOS WHERE id_departamento = ?', [id_departamento])
+
+      if (result.affectedRows === 0) {
+        return res.status(404).json({ message: 'Record not found.' })
+      }
+
+      res.status(200).json({ message: 'Record deleted.', details: result})
+
+    } catch(e) {
+      res.status(500).json({ message: 'Error on delete the record.', exception: e})
+    }
+  })
 }
 
 export default departamentosRoutes
